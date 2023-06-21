34124ccc191d5b444acf1bd557984ec14ce94066
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-06-21 16:56:50 +0000

    first push

---
+++ utils.c
@@ -0,0 +1,75 @@
+#define  _GNU_SOURCE
+#include "monty.h"
+/**
+ * open_file - process the whole monty file
+ * @filename: str name of monty opcode file
+ * @stack: double pointer to top of stack data struct
+ * Return: return an error code or success
+ **/
+int open_file(char *filename, stack_t **stack)
+{
+	size_t len;
+	ssize_t read;
+	unsigned int line_number = 1;
+	char *line = NULL;
+	FILE *fp;
+	char *op;
+
+	if (!filename)
+	{
+		printf("Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		printf("Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	atexit(free_all);
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		/*printf("%s", line); */
+		op = strtok(line, DELIMS);
+		/* printf("<TOKEN1> %s  %u<--\n", op, line_number); */
+		line_number++;
+		/* printf("%s (%u) ", op, line_number); */
+		if (op)
+			get_op(stack, op, line_number);
+	}
+	free(line);
+	fclose(fp);
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * get_op - based on tokens from single line, decide what function to call
+ * @stack: double pointer to head of stack data structure
+ * @op: operator, aka token[0] from getline
+ * @line_number: line in byte-code file being processed, zero indexed
+ * Return: int for mysterious purposes
+ **/
+void get_op(stack_t **stack, char *op, unsigned int line_number)
+{
+	int i = 0;
+	instruction_t all_ops[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; all_ops[i].opcode; i++)
+	{
+		if (strcmp(op, all_ops[i].opcode) == 0)
+		{
+			all_ops[i].f(stack, line_number);
+			return;
+		}
+	}
+	if (strlen(op) != 0 && op[0] != '#')
+	{
+		printf("L%u: unknown instruction %s\n", line_number, op);
+		exit(EXIT_FAILURE);
+	}
+	/* success if you got this far */
+}
